7d9410c34fce6ff17cafa40dac5d741ca7cc0731
GRANIT <gbrowni1@uwyo.edu>
2024-10-20 09:37:08 +0000

    Completed Lab 6

--- cosc1010-lab06.py
+++ cosc1010-lab06.py
@@ -3,8 +3,7 @@
 # Submission Date: 10.10.24
 # Lab 06
 # Lab Section: 
-# Sources, people worked with, help given to: 
-
+# Sources, people worked with, help given to: Ben(TA), Beau Browning(my father), Google
 
 random_string = """
 jppamiqxegokaizvkyawwurhewtcxohryzptznyuedhhmawpic
@@ -60,52 +59,64 @@ zkdenxczyooloczcaahnkehbwimvieedpdlqfafbqvxvfmvabd
 """
 random_string = random_string.replace("\n","") #remove all newline characters
 print(len(random_string)) # Print out the size for reference 
-
-
-
+print("*"*75) #Spacer
 # Above is a string with 2500 characters.
-# Create a program that goes through and counts the occurrence of each character, excluding \n using a  dictionary
-
-#r_string_cnt = {x:random_string.count(x) for x in set(random_string)}
-#print(str(r_string_cnt))
-
+# Create a program that goes through and counts the occurrence of each character, excluding \n using a dictionary
 randostring_count = {}
-for ele in random_string:
-    if ele in randostring_count:
-        randostring_count[ele] += 1
-    else:
-        randostring_count[ele] = 1
-print(str(randostring_count))
-print(sorted(str(randostring_count)))
-# Output each letter and its corresponding occurrence in alphabetical order
-# Output which letter occurred the most 
-# Output which letter occurred the least 
-# Output what the percentage of the string each character is, again in alphabetical
 
+for item in sorted(random_string):
+    if item not in randostring_count:
+            randostring_count[item] = 0
+    randostring_count[item] += 1
+          
+# Output each letter and its corresponding occurrence in alphabetical order     
+# **Done with sorted(random_string) above since we do not need the letters in non-alphabetical order**
+
+print(randostring_count)
+print(f"The sum of these values is: {sum(randostring_count.values())}")
+print("*"*75) #Spacer
+# Output which letter occurred the most
+letter_most = max(randostring_count, key = randostring_count.get)
+print(letter_most)
+# Output which letter occurred the least 
+letter_least = min(randostring_count, key = randostring_count.get)
+print(letter_least)
+# Output what the percentage of the string each character is, again in alphabetical 
+# **Already sorted above
+randostring_percent = {}
+
+for key, value in randostring_count.items():
+    percentage = (value / 2500) * 100
+    randostring_percent[key] = percentage
+
+print(randostring_percent)
+print(f"The sum of these percentages is: {sum(randostring_percent.values())}%") 
 #Tips and trick:
 # You can iterate through strings like you would a list
 # All characters are lowercase 
 # Each letter will be PAIRED with its corresponding value 
 # That is to say, this is a great use of dictionaries
-    # You will  need to add the letter to the dictionary on first occurrence 
+    # You will need to add the letter to the dictionary on first occurrence 
     # Then increment its corresponding count 
 
 
 #Load all the elements into a dictionary
-#Will need to first declare a dictionary 
+#Will need to first declare a dictionary
+print("*"*75) #Spacer
+full_dict = {}
+full_dict["Counts"] = randostring_count
+full_dict["Percentages"] = randostring_percent
+
+#full_dict.append(randostring_count)
+#full_dict.append(randostring_percent)
 
 # Output: each letter and its corresponding occurrence in alphabetical order
+print(full_dict)
+print("*"*75) #Spacer
 
-print("*"*75)
 # Output which letter occurred the most 
+print(f"The letter that occurred the most is: {letter_most} ")
+print("*"*75) #Spacer
 
-most_occurred = ""
-least_occurred = ""
-
-print(f"The letter that occurred the most is {most_occurred}")
-print("*"*75)
 # Output which letter occurred the least 
-print(f"The letter that occurred the most is {least_occurred}")
-print("*"*75)
-
-# Output what the percentage of the string each character is, again in alphabetical
+print(f"The letter that occurred the least is: {letter_least}")
